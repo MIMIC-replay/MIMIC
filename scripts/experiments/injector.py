cc56db7b3a3f94220359e69e1b9a84b2e39f8911
Veronika Todd <veronika.todd@gmail.com>
2024-03-06 12:18:59 -0500

    feat: in our injector, we now create JavaScript files that are linked in the injected HTML script
    Co-authored-by: Lucas Sorribes <lucas.sorribes@gmail.com>

--- scripts/experiments/injector.py
+++ scripts/experiments/injector.py
@@ -7,7 +7,7 @@ def install(raw_file):
     soup = BeautifulSoup(html_content, 'html.parser')
 
     script_element = soup.new_tag('script')
-    script_element['src'] = "Hi Lucas!"
+    script_element['src'] = "./example.js"
     script_element['class'] = 'Owl'
     soup.head.extend([
       Comment('Owl Start'), 
@@ -20,6 +20,12 @@ def install(raw_file):
     with open(raw_file, 'w') as file:
       file.write(str(soup))  
 
+def create_js():
+  with open('example.js', 'w') as file:
+    file.write("console.log('Veronika!')")
+
 files = glob.glob('./**/*.html', recursive=True)
 for file in files:
   install(file)
+
+create_js()
