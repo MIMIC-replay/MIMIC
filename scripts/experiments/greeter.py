4b2595c54de070c2be955fcc5c08d22b1c8bfaed
Veronika Todd <veronika.todd@gmail.com>
2024-03-06 11:59:07 -0500

    feat: experimenting with flags and arguments in python scripting
    Co-authored-by: Lucas Sorribes <lucas.sorribes@gmail.com>

--- scripts/experiments/greeter.py
+++ scripts/experiments/greeter.py
@@ -1,4 +1,9 @@
-import subprocess
+import subprocess, sys
 
-subprocess.run(['echo', "Hoot Hoot!"])
-subprocess.run(['touch', 'virus.c'])
+def process():
+  subprocess.run(['echo', "Hoot Hoot!"])
+  # subprocess.run(['touch', 'evil.c'])
+  print(sys.argv[1])
+
+if __name__ == '__main__':
+  process()
