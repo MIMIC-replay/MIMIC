62d4614b369f0ab613c644aad3210118346340b2
Lucas S <lucas.sorribes@gmail.com>
2024-03-12 22:39:40 +0100

    feat: FRONTEND: begin experiments with rr-player

--- frontend/src/components/MainContentArea.jsx
+++ frontend/src/components/MainContentArea.jsx
@@ -1,12 +1,26 @@
 import ExtraInfo from "./ExtraInfo"
 
+import PlayerTest from "./PlayerTest"
+
 const MainContentArea = () => {
   return (
     <section className="main-content-area">
     <header>🔒/🔓 - Target App Url - 1200 x 800 (viewport size) - some other info??</header>
-    <div className="player">Player <div className="screen">Screen</div></div>
-    <div className="player-controls">Player Controls<br></br><div className="controls"><span>⏪⏯⏩</span><span className="right-controls">📶🔁</span></div></div>
+
+    {/* <div className="player">Player <div className="screen">Screen</div></div>
+    <div className="player-controls">Player Controls<br></br>
+      <div className="controls">
+        <span>⏪⏯⏩</span>
+          <span className="right-controls">📶🔁</span>
+      </div>
+    </div> */}
+        
+    <PlayerTest/>
+
+
     {/* <div className="extra-info">Extra info (Console Logs, Errors, Network)</div> */}
+
+
     <ExtraInfo/>
     <div className="main-right-bar">Main Right Bar</div>
   </section>
