b970d97807aee11bb5075d7494e1304c61802b06
Lucas S <lucas.sorribes@gmail.com>
2024-03-12 12:39:42 +0100

    chore: FRONTEND: add basic React component files

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,9 @@
 // import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import './style/reset.css'
+import './style/variables.css'
+import './style/app.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
