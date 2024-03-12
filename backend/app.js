22dc80d8a1e5cbeb0624d70884fced3e3a8b69d4
Louis-Mascari <Lmascari16@gmail.com>
2024-03-12 09:17:51 -0400

    Fix - middleware use was above route, moved below so all routes are using errorHandler middleware
    Co-authored-by: Lucas Sorribes <lucas.sorribes@gmail.com>

--- backend/app.js
+++ backend/app.js
@@ -13,8 +13,8 @@ app.use(express.json());
 app.use(morgan(":method :url :status :body"));
 const testRouter = require("./controllers/test.js");
 const recordRouter = require("./controllers/record.js");
-app.use(middleware.errorHandler);
 app.use("/api/record", recordRouter);
 app.use("/api/test", testRouter);
 
+app.use(middleware.errorHandler); // needs to be below all routes for all to use
 module.exports = app;
