1cf6252c747fcd24451e03dd89410f9e0ffb6ff8
Veronika Todd <veronika.todd@gmail.com>
2024-03-13 14:49:02 -0400

    feat: integrated psql connection into backend scaffolding

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 
 PORT = process.env.PORT;
+PSQL_PASSWORD= process.env.PSQL_PASSWORD;
+POSTGRES_PORT= process.env.POSTGRES_PORT;
 
-module.exports = { PORT };
+module.exports = { PORT, POSTGRES_PORT, PSQL_PASSWORD };
