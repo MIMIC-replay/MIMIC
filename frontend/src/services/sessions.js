d35c6d53527d611d39e699f746fbe62428a7db39
Lucas S <lucas.sorribes@gmail.com>
2024-03-13 20:53:57 +0100

    build: FRONTEND: begin adding sessions services

--- frontend/src/services/sessions.js
+++ frontend/src/services/sessions.js
@@ -1,9 +1,16 @@
 import sessions from '../../mock-data/sessions'
 
+const PORT = 3001
+const BASE_URL = `http://localhost:${PORT}/api/projects`
+
 const getSessions = async () => {
-  // simulates an API call
-  const response = await sessions
-  return response
+  try {
+    // simulates an API call
+    const response = await sessions
+    return response
+  } catch (e) {
+    console.log(e)
+  }
 }
 
 
