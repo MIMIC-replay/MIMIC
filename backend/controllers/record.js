b53a601569b3d6e80df350d36debea6bedf85412
Veronika Todd <veronika.todd@gmail.com>
2024-03-13 15:43:06 -0400

    fix: one session only occupies one row in the database, with the session having a unique ID

--- backend/controllers/record.js
+++ backend/controllers/record.js
@@ -7,9 +7,10 @@ const postgres = require("../models/postgres.js")
 const fflate = require('fflate');
 
 const allRecordedEvents = [];
+let sessionIndex;
 
 recordRouter.get("/", (req, res) => { // method to be deleted when we migrate from sandbox frontend to true frontend
-  postgres.db.one("SELECT encode(session_data::bytea, 'escape') as session_data FROM sessions WHERE id = $1", [currentSessionId], session => session.session_data)
+  postgres.db.one("SELECT encode(session_data::bytea, 'escape') as session_data FROM sessions WHERE id = $1", [3], session => session.session_data)
     .then(data => JSON.parse(data))
     .then(data => Object.keys(data).map((key) => data[key]))
     .then(data => new Uint8Array(data))
@@ -28,15 +29,27 @@ recordRouter.post("/", (req, res) => {
 
   const compressedEventsString = fflate.strToU8(JSON.stringify(allRecordedEvents));
   const allEventsCompressed = fflate.compressSync(compressedEventsString, { level: 6})
-  postgres.db.one('INSERT INTO sessions(session_data) VALUES($1) RETURNING id', [allEventsCompressed], session => session.id)
+  
+  if (sessionIndex) {
+    postgres.db.any('UPDATE sessions SET session_data = $1 WHERE id = $2', [allEventsCompressed, sessionIndex])
     .then(data => {
-        currentSessionId = data;
         res.sendStatus(200);
-        console.log(`Successfully added events to database. Current ID is ${currentSessionId}`)
+        console.log(`Successfully updated session ${sessionIndex} events in PostgreSQL.`)
     })
     .catch((error) => {
-      console.log('Unable to send event data to PostgreSQL:', error.message);
+      console.log(`Unable to update session ${sessionIndex} events in PostgreSQL:`, error.message);
     });
+  } else {
+    postgres.db.one('INSERT INTO sessions(session_data) VALUES($1) RETURNING id', [allEventsCompressed], session => session.id)
+    .then(data => {
+        sessionIndex = data;
+        res.sendStatus(200);
+        console.log(`Successfully added new session to database. Current ID is ${sessionIndex}`)
+    })
+    .catch((error) => {
+      console.log('Unable to add new session to PostgreSQL:', error.message);
+    });
+  }
 });
 
 
