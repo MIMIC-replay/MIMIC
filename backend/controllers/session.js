c47f68e7f80ea5d0789eed319ef2b1cb331e4926
Veronika Todd <veronika.todd@gmail.com>
2024-03-14 12:13:11 -0400

    fix: now identifying metadata extracted from the database by column name rather than fkey identifier

--- backend/controllers/session.js
+++ backend/controllers/session.js
@@ -17,7 +17,7 @@ const extractNetworkEvents = (eventsArr) => {
 */
 
 sessionsRouter.get('/:projectId', async (req, res) => {
-  // - request all session associated with a project id
+  // requests all sessions associated with a project id
   for (index = 0; index < validSessionIds.length; index++) {
     const sessionId = validSessionIds[index];
     const session = {id: sessionId};
@@ -37,21 +37,21 @@ sessionsRouter.get('/:projectId', async (req, res) => {
     session.network = extractNetworkEvents(events);
     session.logs = extractConsoleEvents(events)
     */
-    let rawMetadata = await postgres.db.one("SELECT ROW_TO_JSON(ROW(session_start, ip_address, url, city, region, country, os_name, os_version, browser_name, browser_version, https_protected, viewport_height, viewport_width)) FROM sessions WHERE id = $1", [sessionId])              
+   
+    let rawMetadata = await postgres.db.one("SELECT session_start, ip_address, url, city, region, country, os_name, os_version, browser_name, browser_version, https_protected, viewport_height, viewport_width FROM sessions WHERE id = $1", [sessionId])              
                       .catch((error) => {
                         console.log('Unable to retrieve event data from PostgreSQL:', error.message);
                         res.json(error)
                       });
 
-    rawMetadata = rawMetadata["row_to_json"];
-    metadata.ip = rawMetadata["f2"];
-    metadata.location = rawMetadata["f4"] + ", " + rawMetadata["f5"] + ", " + rawMetadata["f6"];
-    metadata.os = { name: rawMetadata["f7"], version: rawMetadata["f8"]};
-    metadata.browser = { name: rawMetadata["f9"], version: rawMetadata["f10"]};
-    metadata.https = rawMetadata["f11"];
-    metadata.viewport = { height: rawMetadata["f13"], width: rawMetadata["f12"]};
-    metadata.date = rawMetadata["f1"];
-    metadata.url = rawMetadata["f3"];
+    metadata.ip = rawMetadata.ip_address;
+    metadata.location = rawMetadata.city + ", " + rawMetadata.region + ", " + rawMetadata.country;
+    metadata.os = { name: rawMetadata.os_name, version: rawMetadata.os_version };
+    metadata.browser = { name: rawMetadata.browser_name, version: rawMetadata.browser_version };
+    metadata.https = rawMetadata.https_protected;
+    metadata.viewport = { height: rawMetadata.viewport_height, width: rawMetadata.viewport_width };
+    metadata.date = rawMetadata.session_start;
+    metadata.url = rawMetadata.url;
 
     session.metadata = metadata
     sessions.push(session)
