2233db5a1c1870fd4dfd48fca508091a96a307f3
Lucas S <lucas.sorribes@gmail.com>
2024-03-14 23:39:56 +0100

    build: FRONTEND: install react router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,6 @@
 // import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom";
 
 import App from './App'
 
@@ -11,7 +12,7 @@ import './style/icons.css'
 import './style/unminified-rrplayer.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
+  <BrowserRouter>
     <App />
-  /* </React.StrictMode>, */
+  </BrowserRouter>
 )
