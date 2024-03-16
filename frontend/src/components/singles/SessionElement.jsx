4c1f73aa4b690fbbf925a43f83ca2bf24b0c15ff
Lucas S <lucas.sorribes@gmail.com>
2024-03-17 00:34:47 +0100

    feat: FRONTEND: now minichart in session elements list represents latency

--- frontend/src/components/singles/SessionElement.jsx
+++ frontend/src/components/singles/SessionElement.jsx
@@ -7,20 +7,26 @@ import {
   // Routes 
 } from 'react-router-dom'
 
-const data = [
-  {uv: 10, pv: 400, amt: 400 },
-  {uv: 120, pv: 2520, amt: 1520 },
-  {uv: 440, pv: 2640, amt: 2640 },
-  {uv: 60, pv: 760, amt: 760 },
-  {uv: 80, pv: 2880, amt: 2880 },
-  {uv: 500, pv: 3000, amt: 3000 },
-];
+// const data = [
+//   {uv: 10, pv: 400, amt: 400 },
+//   {uv: 120, pv: 2520, amt: 1520 },
+//   {uv: 440, pv: 2640, amt: 2640 },
+//   {uv: 60, pv: 760, amt: 760 },
+//   {uv: 80, pv: 2880, amt: 2880 },
+//   {uv: 500, pv: 3000, amt: 3000 },
+// ];
 
 // const SessionElement = ({session, setSelectedSession}) => {
 const SessionElement = ({session}) => {
+  
+  const data = session.events.map(e =>{
+    return {...e.data}
+  })
+  
+  console.log(data)
   const renderLineChart = (
     <LineChart width={200} height={50} data={data}>
-      <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+      <Line type="monotone" dataKey="latency" stroke="#8884d8" />
     </LineChart>
   );
 
