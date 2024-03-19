2147731f5c4077181550b05deaf02b1203d78f93
Lucas S <lucas.sorribes@gmail.com>
2024-03-19 20:24:35 +0100

    feat: add tokenExtractor and userExtractor middleware to utils/midleware.js

--- backend/utils/middleware.js
+++ backend/utils/middleware.js
@@ -46,4 +46,30 @@ const errorHandler = (error, request, response, next) => {
   next(error);
 };
 
-module.exports = { errorHandler, sessionCookie };
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+
+  if (authorization && authorization.startsWith('Bearer ')) {
+    request.token = authorization.replace('Bearer ', '')
+  } else request.token = null
+
+  next()
+}
+
+const userExtractor = async (request, response, next) => {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+  request.user = await User.findById(decodedToken.id)
+
+  next()
+}
+
+module.exports = { 
+  errorHandler, 
+  sessionCookie,   
+  tokenExtractor,
+  userExtractor 
+};
