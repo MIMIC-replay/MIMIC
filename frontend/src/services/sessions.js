dd93fdccf76df4dc38077746b7e547bfb9eaebd5
Veronika Todd <veronika.todd@gmail.com>
2024-03-19 14:13:46 -0400

    fix: updates sessions route to make API call to backend. switches to making API call to backend instead of accessing hard coded data. adds axios and react-router-dom to frontend package.json.
    Co-authored-by: ErikWiens <erik.wiens@gmail.com>
    Co-authored-by: Lucas Sorribes <lucas.sorribes@gmail.com>
    Co-authored-by: Louis Mascari <lmascari16@gmail.com>

--- frontend/src/services/sessions.js
+++ frontend/src/services/sessions.js
@@ -1,27 +1,20 @@
-import sessions from '../../mock-data/TESTsessions.json'
+// import sessions from '../../mock-data/TESTsessions.json'
 import axios from 'axios'
 
 const PORT = 3001
-const BASE_URL = `http://localhost:${PORT}/api/projects`
+const BASE_URL = `http://localhost:${PORT}/api/project`
 
 const getSessions = async (projectId) => {
   // simulates an API call
 
-  const config = {
-    headers: {
-      'ngrok-skip-browser-warning': 'true'
-    }
-  }
-
   try {
-    // const response = await axios.get(
-    //   'https://468f-38-99-108-149.ngrok-free.app/api/project/986953cc-b0d6-4a54-a026-0bad9a629656',
-    //   config
-    // )
-    // console.log(response.data.sessions)
+    const response = await axios.get(
+      `${BASE_URL}/986953cc-b0d6-4a54-a026-0bad9a629656`
+     )
+     console.log(response.data.sessions)
 
-    // return response.data.sessions
-
+     return response.data.sessions
+/*
     return [sessions[0], {...sessions[0], id:"766ygdf-6546465d-gdfgfd-6565g5gg", 
         events: [
           ...sessions[0].events, 
@@ -29,7 +22,7 @@ const getSessions = async (projectId) => {
         ]
       }
     ]
-
+*/
   } catch (e) {
     console.error(e)
     return []
