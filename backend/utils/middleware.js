8d9f5243939eb38e69f532dd5f652040259412df
Lucas S <lucas.sorribes@gmail.com>
2024-03-19 20:46:20 +0100

    feat: expand errorHandler to include more errors

--- backend/utils/middleware.js
+++ backend/utils/middleware.js
@@ -39,8 +39,16 @@ const sessionCookie = (req, res, next) => {
 const errorHandler = (error, request, response, next) => {
   logger.error(error.message);
 
-  if (error.name === "TokenExpiredError") {
-    return response.status(401).json({ error: "Your token has expired." });
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  } else if (error.name ===  'JsonWebTokenError') {
+    return response.status(401).json({ error: error.message })
+  } else if (error.name === 'TokenExpiredError') {
+    return response.status(401).json({
+      error: 'token expired'
+    })
   }
 
   next(error);
