ad3b3ee2c310cc6e6247e999faf7c307b150d9a3
Louis-Mascari <Lmascari16@gmail.com>
2024-03-19 20:55:33 -0400

    improvement: saving events no longer creates a captured network event object to reduce spam (this is reflected in mimicScript as well). Reduces erroneous routes in targetApp that was triggering what appeared to be cookie validation errors, includes a server within targetApp/src/server to simualte websocket communication for further testing (will need to run targetApp's server prior to running targetApp if want to capture this communication).

--- sandbox/server/server.js
+++ sandbox/server/server.js
@@ -3,12 +3,17 @@ const cors = require("cors");
 const app = express();
 const port = 3001;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: true, //"http://localhost:5173", // this we have to figure out, we can't manually add the origin in the backend for all target applications
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 const allRecordedEvents = [];
 
-app.post("/record", (req, res) => {
+app.post("/api/record", (req, res) => {
   const batchOfEvents = req.body;
   // let consolePayloads = batchOfEvents.filter((obj) => obj.data.plugin);
   // if (consolePayloads.length > 0) {
@@ -34,14 +39,6 @@ app.post("/record", (req, res) => {
 
 app.get("/allRecordedEvents", (req, res) => {
   res.json(allRecordedEvents);
-});
-
-app.get("/random", (req, res) => {
-  res.json("aslkdfgnalksgnalksdgnaksl");
-});
-
-app.delete("/deleteTest", (req, res) => {
-  res.status(500).json("deleted");
 });
 
 app.listen(port, () => {
