bbe8398c35ca0b3ab8817549840f61bdac1d8b19
Lucas S <lucas.sorribes@gmail.com>
2024-03-22 00:41:30 +0100

    feat: event list now shows decoded types for events Co-authored-by: Veronika Todd <veronika.todd@gmail.com> Co-authored-by: Erik Wiens <erik.wiens@gmail.com> Co-authored-by: Louis Mascari <lmascari16@gmail.com>

--- frontend/src/components/singles/RightBarEventListElement.jsx
+++ frontend/src/components/singles/RightBarEventListElement.jsx
@@ -1,11 +1,23 @@
-import { relativeTime } from "../../helpers/dataExtractors"
+import { eventAnalyzer, relativeTime } from "../../helpers/dataExtractors"
 
 const RightBarEventListElement = ({event, session}) =>{
 
+  const {
+    numberType,
+    decodedType,
+    isUserInteraction,
+    interactionSource,
+    isMouseInteraction,
+    mouseInteraction,
+  } = eventAnalyzer(event)
+
   const time = relativeTime(event, session)
   return (
-    <li>
-      {`Time: ${time} - Type: ${event.type}`}
+    <li className="right-bar-event-element">
+      <div>
+        <p>{time}</p>
+        <p>{decodedType}</p>
+      </div>
     </li>
   )  
 }
