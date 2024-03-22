f54ae698a1ef5c4c9638aaadf47ebf88e3e58b95
Lucas S <lucas.sorribes@gmail.com>
2024-03-22 14:06:23 +0100

    refactor: move icons to independent components

---
+++ frontend/src/components/iconComponents/duration.jsx
@@ -0,0 +1,9 @@
+const DurationIcon = () => {
+  return (
+    <svg width="24" height="24" className="feather icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M5.06152 12C5.55362 8.05369 8.92001 5 12.9996 5C17.4179 5 20.9996 8.58172 20.9996 13C20.9996 17.4183 17.4179 21 12.9996 21H8M13 13V9M11 3H15M3 15H8M5 18H10" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+  )
+}
+
+export default DurationIcon
