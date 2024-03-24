12655b2aa8ef903395fc9adc4c1352a356e08eed
Lucas S <lucas.sorribes@gmail.com>
2024-03-24 21:28:06 +0100

    improvement: move loadSessionPlayer function to useEffect callback

--- frontend/src/components/PlayerTest.jsx
+++ frontend/src/components/PlayerTest.jsx
@@ -1,44 +1,78 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 import rrwebPlayer from 'rrweb-player';
 import 'rrweb-player/dist/style.css';
 // import '../style/unminified-rrplayer.css'
 
 const PlayerTest = ({session}) => {
+  const [player, setPlayer] = useState(null)
 
-  // return null
-  // console.log(session)
   useEffect(() => {
-    if (session.events.length < 1) return 
-    loadSessionPlayer()
-  })
+    if (session.events.length < 1) return
 
-  const loadSessionPlayer = async () => {
-    let replayerDiv = document.getElementById("replayer")
+    (async () => {
 
-    if (replayerDiv.firstChild) {
-      replayerDiv.removeChild(replayerDiv.firstChild)
-    }
+      let replayerDiv = document.getElementById("replayer")
 
-    try {
-      new rrwebPlayer({
-        target: document.getElementById("replayer"),
-        props: {
-          events: session.events,
-          // showController: false,
-          width: 600,
-          height: 270,
-          autoPlay: true,
-        },
-      })
+      if (replayerDiv.firstChild) {
+        replayerDiv.removeChild(replayerDiv.firstChild)
+      }
+  
+      try {
+        const newPlayer = 
+          new rrwebPlayer({
+            target: document.getElementById("replayer"),
+            props: {
+              events: session.events,
+              // showController: false,
+              width: 600,
+              height: 270,
+              autoPlay: true,
+            },
+          })
+        
+        setPlayer(newPlayer)
+        // return newPlayer
+      } catch (error) {
+        console.log(error)
+      }
 
-    } catch (error) {
-      console.log(error)
-    }
-  }
+      // const newPlayer = await loadSessionPlayer()
+    })() 
+  }, [session])
+
+  // const loadSessionPlayer = async () => {
+  //   let replayerDiv = document.getElementById("replayer")
+
+  //   if (replayerDiv.firstChild) {
+  //     replayerDiv.removeChild(replayerDiv.firstChild)
+  //   }
+
+  //   try {
+  //     const newPlayer = 
+  //       new rrwebPlayer({
+  //         target: document.getElementById("replayer"),
+  //         props: {
+  //           events: session.events,
+  //           // showController: false,
+  //           width: 600,
+  //           height: 270,
+  //           autoPlay: true,
+  //         },
+  //       })
+      
+  //     return newPlayer
+
+  //   } catch (error) {
+  //     console.log(error)
+  //   }
+  // }
 
   return (
-    <div id="replayer"></div>    
+    <div className="player-area"> 
+      <div id="replayer"></div>    
+      <div className="player-controls"></div>    
+    </div>
   )
 }
 
