09976a16f45b905ef7c5ea5e3eea455ea571d8d6
Lucas S <lucas.sorribes@gmail.com>
2024-03-26 09:32:12 +0100

    chore: remove unnecessary logs and comments

--- frontend/src/components/MainContentArea.jsx
+++ frontend/src/components/MainContentArea.jsx
@@ -1,21 +1,10 @@
-// import { useState, useEffect } from "react"
-
 import ExtraInfo from "./ExtraInfo"
 import MainContentRightBar from "./MainContentRightBar"
 import SessionContentHeader from './singles/SessionContentHeader'
-import PlayerArea from "./PlayerArea"
-import { useEffect, useState } from "react"
+// import PlayerArea from "./PlayerArea"
 import PlayerTest from "./PlayerTest"
 
 const MainContentArea = ({session}) => {
-  // const [events, setEvents] = useState([])
-
-  // useEffect(() => {
-  //   if (!session) return 
-  // }, [session])
-
-  // useEffect()
-
   if (!session) return
 
   return (
