f0a163c0fbfbae3c35bee31e4dd375432d61e79b
Lucas S <lucas.sorribes@gmail.com>
2024-03-27 01:21:16 +0100

    chore: remove mock data development environment
    Co-authored-by: Erik Wiens <erik.wiens@gmail.com>
    Co-authored-by: Louis Mascari <lmascari16@gmail.com>
    Co-authored-by: Veronika Todd <veronika.todd@gmail.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,11 +24,7 @@ function App() {
   const [sessions, setSessions] = useState([])
   const [sessionsInList, setSessionsInList] = useState([])
   const [notification, setNotification] = useState(null)
-  // const [project, setProject] = useState(null)
-
-  // DEVELOPMENT:
-  const [project, setProject] = useState({id: 1234123, name: 'super_project'})
-
+  const [project, setProject] = useState(null)
 
   const match = useMatch('/sessions/:id')
 
