4dc0aadccff4601ef600b062ba0dbf1131dba92d
Lucas S <lucas.sorribes@gmail.com>
2024-03-27 02:10:08 +0100

    fix: fix keeping state after refreshing reverting to old App.jsx version

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,7 +24,11 @@ function App() {
   const [sessions, setSessions] = useState([])
   const [sessionsInList, setSessionsInList] = useState([])
   const [notification, setNotification] = useState(null)
-  const [project, setProject] = useState(null)
+  // const [project, setProject] = useState(null)
+
+  // DEVELOPMENT:
+  const [project, setProject] = useState({id: 1234123, name: 'super_project'})
+
 
   const match = useMatch('/sessions/:id')
 
