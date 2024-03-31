d0c5ab221a4088c914163e9a7fdf56f020b7a0cb
Lucas S <lucas.sorribes@gmail.com>
2024-03-31 18:06:44 +0200

    style: restyle site header format

--- frontend/src/components/singles/SiteHeader.jsx
+++ frontend/src/components/singles/SiteHeader.jsx
@@ -3,7 +3,7 @@ const SiteHeader = ({handleLogout, project}) => {
     <header className="site-header">
       <img src="/transparent-logo.png" className="site-logo" alt="Mimic Logo"></img>
       <p className="project-name">{project.name}</p>
-      <p className="project-id">{`- ${project.id}`}</p>
+      <p className="project-id">{`:: ${project.id}`}</p>
       <button className="logout-button" onClick={handleLogout}>Log out</button>
     </header>
   )
