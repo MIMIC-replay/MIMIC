e8df5edf79562ce25d2811e13f82c87e126cff6e
Lucas S <lucas.sorribes@gmail.com>
2024-03-31 16:21:40 +0200

    improvement: base url and port now stored in .env.local file

--- frontend/src/services/login.js
+++ frontend/src/services/login.js
@@ -1,9 +1,11 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/login'
+
+const PORT = import.meta.env.VITE_REACT_APP_PORT
+const BASE_URL = `${import.meta.env.VITE_REACT_APP_BASE_URL}:${PORT}/api/login`
 
 export const login = async (credentials) => {
   try {
-    const response = await axios.post(baseUrl, credentials)
+    const response = await axios.post(BASE_URL, credentials)
     return response.data
   } catch (e) {
     console.error(e.message)
