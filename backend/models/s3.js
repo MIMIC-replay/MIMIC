b1c92e881262cd22f64b96e9bc98929e76a15c13
Louis-Mascari <Lmascari16@gmail.com>
2024-04-02 09:44:25 -0400

    feat: uses AWS S3 as object store in place of minio for production.
    Co-authored-by: Erik Wiens <erik.wiens@gmail.com>
    Co-authored-by: Lucas Sorribes <100610401+devluxor@users.noreply.github.com>
    Co-authored-by: Veronika Todd <veronika.todd@gmail.com>

---
+++ backend/models/s3.js
@@ -0,0 +1,70 @@
+const {
+  S3Client,
+  PutObjectCommand,
+  GetObjectCommand,
+} = require("@aws-sdk/client-s3");
+const config = require("../utils/config.js");
+
+const s3 = new S3Client({
+  region: "us-east-1",
+  credentials: {
+    accessKeyId: config.AWS_ACCESS_KEY_ID,
+    secretAccessKey: config.AWS_SECRET_ACCESS_KEY,
+  },
+});
+
+// Function to upload data to S3
+function uploadToEventStorage(sessionId, allEventsCompressed) {
+  const buffer = Buffer.from(allEventsCompressed);
+  return new Promise(async (resolve, reject) => {
+    try {
+      const params = {
+        Bucket: config.AWS_BUCKET_NAME,
+        Key: sessionId,
+        Body: buffer,
+      };
+      const command = new PutObjectCommand(params);
+      const data = await s3.send(command);
+      console.log("Data uploaded successfully");
+      resolve(data);
+    } catch (err) {
+      console.error("Error uploading data:", err);
+      reject(err);
+    }
+  });
+}
+
+// Function to retrieve object content
+async function getObjectContent(objectName) {
+  const params = {
+    Bucket: config.AWS_BUCKET_NAME,
+    Key: objectName,
+  };
+
+  return new Promise(async (resolve, reject) => {
+    try {
+      const command = new GetObjectCommand(params);
+      const response = await s3.send(command);
+
+      let responseDataChunks = [];
+
+      // Handle an error while streaming the response body
+      response.Body.on("error", (err) => reject(err));
+
+      // Attach a 'data' listener to add the chunks of data to our array
+      // Each chunk is a Buffer instance
+      response.Body.on("data", (chunk) => responseDataChunks.push(chunk));
+
+      response.Body.on("end", () => {
+        const content = Buffer.concat(responseDataChunks);
+        console.log(typeof content, content);
+        resolve(content);
+      });
+    } catch (err) {
+      console.error("Error retrieving object content:", err);
+      throw err;
+    }
+  });
+}
+
+module.exports = { uploadToEventStorage, getObjectContent };
