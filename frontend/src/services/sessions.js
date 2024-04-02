ad5e9f1991608ea946086d51beca29c7dcf24943
Lucas S <lucas.sorribes@gmail.com>
2024-04-02 16:25:38 +0200

    feat: change base url for login.js and sessions.js for server serving static files Co-authored-by: Louis Mascari <lmascari16@gmail.com>

--- frontend/src/services/sessions.js
+++ frontend/src/services/sessions.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
-const PORT = import.meta.env.VITE_REACT_APP_PORT
-const BASE_URL = `${import.meta.env.VITE_REACT_APP_BASE_URL}:${PORT}/api/project`
+// const PORT = import.meta.env.VITE_REACT_APP_PORT
+// const BASE_URL = `${import.meta.env.VITE_REACT_APP_BASE_URL}:${PORT}/api/project`
+const BASE_URL = '/api/project'
 
 let token = null 
 
