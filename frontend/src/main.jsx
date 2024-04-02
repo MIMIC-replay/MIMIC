5def492eeda3a423a00f4b321f7c35bb6dbb3ee5
Lucas S <lucas.sorribes@gmail.com>
2024-04-02 16:26:52 +0200

    feat: replace React BrowserRouter with HashRouter Co-authored-by: Louis Mascari <lmascari16@gmail.com>

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 // import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from "react-router-dom"
+// import { BrowserRouter } from "react-router-dom"
+import { HashRouter } from "react-router-dom"
 import { CustomScroll } from 'react-custom-scroll'
 
 import App from './App'
@@ -15,9 +16,9 @@ import './style/login.css'
 import './style/custom-player-ui.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <CustomScroll>
       <App />
     </CustomScroll>
-  </BrowserRouter>
+  </HashRouter>
 )
