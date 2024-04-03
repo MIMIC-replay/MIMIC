66eeffd1548997493f9985e1ae4fe5f0309e6cbe
Lucas S <lucas.sorribes@gmail.com>
2024-04-03 19:56:48 +0200

    fix: player now keeps state when events are clicked

--- frontend/src/components/PlayerArea.jsx
+++ frontend/src/components/PlayerArea.jsx
@@ -42,14 +42,19 @@ const PlayerArea = ({session}) => {
     })() 
   }, [session])
 
-  const playerNavigate = () => {
+  const playerNavigate = async () => {
     const goToSeconds = searchParams.get('time')
 
     if (!goToSeconds || !player) return
 
-    player.play(); // prevents session replay from restarting from beginning if replay was at end
     player.goto(Math.floor(goToSeconds * 1000));
-    player.pause(); // returns to paused state for UX
+
+    let getCurrentState
+    player.addEventListener('ui-update-player-state', (event) => {
+      getCurrentState = () => event.payload;
+    });
+    
+    if (getCurrentState && getCurrentState() !== 'playing') player.pause();
   }
 
   playerNavigate()
