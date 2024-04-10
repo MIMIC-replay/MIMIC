48f8bd6d1f00e543cd030568dfddff127ba00f0b
Veronika Todd <veronika.todd@gmail.com>
2024-04-10 14:27:08 -0400

    refactor: config file now directly references our chosen location API, to ease installation for MIMIC users.

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 PORT = process.env.PORT;
 PSQL_PASSWORD = process.env.PSQL_PASSWORD;
 POSTGRES_PORT = process.env.POSTGRES_PORT;
-LOCATION_API_URL = process.env.LOCATION_API_URL;
+LOCATION_API_URL = "https://api.findip.net";
 LOCATION_API_TOKEN = process.env.LOCATION_API_TOKEN;
 SECRET = process.env.SECRET;
 REDIS_PASSWORD = process.env.REDIS_PASSWORD;
