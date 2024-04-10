f9136dd7e09314b0429eb068fb5e4b4aa468b743
Veronika Todd <veronika.todd@gmail.com>
2024-04-10 16:45:19 -0400

    chore: removed drop table statements from SQL file, so that users cannot accidentally wipe their entire MIMIC database. Removed unnecessary comments.

--- backend/models/initialize.sql
+++ backend/models/initialize.sql
@@ -5,9 +5,6 @@
     -- postgres-# exit
   -- command line: psql -d mimic < initialize.sql
 
-DROP TABLE IF EXISTS projects CASCADE; -- might want to remove these lines in deployment so users can't easily nuke all their data
-DROP TABLE IF EXISTS sessions CASCADE;
-
 CREATE TABLE projects (
   id UUID PRIMARY KEY,
   created_at timestamp NOT NULL DEFAULT NOW(),
@@ -18,11 +15,8 @@ CREATE TABLE projects (
 CREATE TABLE sessions (
   id UUID PRIMARY KEY,
   project_id UUID NOT NULL REFERENCES projects(id) ON DELETE CASCADE,
-  -- session_data bytea NOT NULL, --compressed blob data
   session_start timestamp NOT NULL DEFAULT NOW(),
   session_end timestamp,
-  -- has_rage_clicks boolean NOT NULL,
-  -- has_errors boolean NOT NULL,
   url varchar(100), 
   ip_address varchar(39),
   city varchar(30),
@@ -36,6 +30,4 @@ CREATE TABLE sessions (
   timezone varchar(30),
   longitude FLOAT,
   latitude FLOAT
-  -- viewport_height integer,
-  -- viewport_width integer
 );
