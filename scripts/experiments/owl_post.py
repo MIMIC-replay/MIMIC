92e5fb6bfc22beaf9ae8a882c25f3bb6ca962424
Veronika Todd <veronika.todd@gmail.com>
2024-04-10 17:03:58 -0400

    chore: cleans up scripts directory, including removal of experiments.

--- scripts/experiments/owl_post.py
+++
@@ -1,9 +0,0 @@
-import subprocess, sys
-
-def process():
-  subprocess.run(['python3', 'config.py'])
-  subprocess.run(['node', 'post_data.owl.js'])
-  subprocess.run(['echo', "Hoot hoot motherbeaker!"])
-
-if __name__ == '__main__':
-  process()
