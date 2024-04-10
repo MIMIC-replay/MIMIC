417619aca561c0cb634d6630b2725c76ed3cf795
Veronika Todd <veronika.todd@gmail.com>
2024-04-10 16:54:41 -0400

    chore: removes sandbox directory.

--- sandbox/targetApp/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import TargetApp from "./TargetApp.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <TargetApp />
-  </React.StrictMode>
-);
