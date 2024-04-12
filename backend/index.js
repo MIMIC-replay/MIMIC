f8c4d06560b5bb31a738d89780095bebbc73e9d1
Veronika Todd <veronika.todd@gmail.com>
2024-04-12 15:30:15 -0400

    chore: improves code quality following eslint usage.

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
-const app = require("./app.js");
-const config = require("./utils/config.js");
-const postgres = require("./models/postgres.js");
-const PORT = config.PORT;
+const app = require('./app');
+const config = require('./utils/config');
+const postgres = require('./models/postgres');
+
+const { PORT } = config;
 
 app.listen(PORT, () => {
   console.log(`MIMIC is running on port ${PORT}`);
